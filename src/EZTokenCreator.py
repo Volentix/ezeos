08ad97f900acf70de9a26bfa72e24f81cb2160b7
Sylvain Cormier <sylvaincormier@protonmail.com>
2018-06-15 16:26:01 -0400

    reformat UI

--- src/EZTokenCreator.py
+++ src/EZTokenCreator.py
@@ -91,6 +91,8 @@ class Order():
     amount = 0.0000
     contract = ""
     currency = ""
+    contractAccountName = ""
+    
     
 class Dialog(QtGui.QDialog):
     
@@ -98,89 +100,94 @@ class Dialog(QtGui.QDialog):
         super(Dialog, self).__init__(parent)
 
         frameStyle = QtGui.QFrame.Sunken | QtGui.QFrame.StyledPanel
-        self.setMinimumSize(1280, 1024)
+        #self.setMinimumSize(1280, 1024)
 
-        self.openContractLabel = QtGui.QLabel()
-        self.openContractLabel.setFrameStyle(frameStyle)
+#        self.openContractLabel = QtGui.QLabel()
+#        self.openContractLabel.setFrameStyle(frameStyle)
         self.openContractButton = QtGui.QPushButton("Open Contract")    
 
-        self.setWalletNameLabel = QtGui.QLabel()
-        self.setWalletNameLabel.setFrameStyle(frameStyle)
+#        self.setWalletNameLabel = QtGui.QLabel()
+#        self.setWalletNameLabel.setFrameStyle(frameStyle)
         self.setWalletNameButton = QtGui.QPushButton("Wallet name")
         
-        self.restartLabel = QtGui.QLabel()
-        self.restartLabel.setFrameStyle(frameStyle)
+#        self.restartLabel = QtGui.QLabel()
+#        self.restartLabel.setFrameStyle(frameStyle)
         self.restartButton = QtGui.QPushButton("Reset chain")
         
-        self.startLabel = QtGui.QLabel()
-        self.startLabel.setFrameStyle(frameStyle)
+#        self.startLabel = QtGui.QLabel()
+#        self.startLabel.setFrameStyle(frameStyle)
         self.startButton = QtGui.QPushButton("Start chain")
         
-        self.stopLabel = QtGui.QLabel()
-        self.stopLabel.setFrameStyle(frameStyle)
+#        self.stopLabel = QtGui.QLabel()
+#        self.stopLabel.setFrameStyle(frameStyle)
         self.stopButton = QtGui.QPushButton("Stop chain")
         
-        self.flushLabel = QtGui.QLabel()
-        self.flushLabel.setFrameStyle(frameStyle)
+#        self.flushLabel = QtGui.QLabel()
+#        self.flushLabel.setFrameStyle(frameStyle)
         self.flushButton = QtGui.QPushButton("Flush wallets")
         
-        self.createWalletLabel = QtGui.QLabel()
-        self.createWalletLabel.setFrameStyle(frameStyle)
+#        self.createWalletLabel = QtGui.QLabel()
+#        self.createWalletLabel.setFrameStyle(frameStyle)
         self.createWalletButton = QtGui.QPushButton("Create wallet")
 
-        self.setOwnerKeyLabel = QtGui.QLabel()
-        self.setOwnerKeyLabel.setFrameStyle(frameStyle)
+#        self.setOwnerKeyLabel = QtGui.QLabel()
+#        self.setOwnerKeyLabel.setFrameStyle(frameStyle)
         self.setOwnerKeyButton = QtGui.QPushButton("Set owner Key")
         
-        self.setActiveKeyLabel = QtGui.QLabel()
-        self.setActiveKeyLabel.setFrameStyle(frameStyle)
+#        self.setActiveKeyLabel = QtGui.QLabel()
+#        self.setActiveKeyLabel.setFrameStyle(frameStyl self.setMinimumSize(1280, 1024)e)
         self.setActiveKeyButton = QtGui.QPushButton("Set active key")
 
-        self.importPrivateKeysLabel = QtGui.QLabel()
-        self.importPrivateKeysLabel.setFrameStyle(frameStyle)
+#        self.importPrivateKeysLabel = QtGui.QLabel()
+#        self.importPrivateKeysLabel.setFrameStyle(frameStyle)
         self.importPrivateKeysButton = QtGui.QPushButton("Import private keys")
         
-        self.setAccountNameLabel = QtGui.QLabel()
-        self.setAccountNameLabel.setFrameStyle(frameStyle)
+#        self.setAccountNameLabel = QtGui.QLabel()
+#        self.setAccountNameLabel.setFrameStyle(frameStyle)
         self.setAccountNameButton = QtGui.QPushButton("Account name")
 
-        self.createAccountLabel = QtGui.QLabel()
-        self.createAccountLabel.setFrameStyle(frameStyle)
+#        self.createAccountLabel = QtGui.QLabel()
+#        self.createAccountLabel.setFrameStyle(frameStyle)
         self.createAccountButton = QtGui.QPushButton("Create account")
 
-        self.openFileNameLabel = QtGui.QLabel()
-        self.openFileNameLabel.setFrameStyle(frameStyle)
-        self.openFileNameButton = QtGui.QPushButton("set Contract Steps")
-
-        self.issueLabel = QtGui.QLabel()
-        self.issueLabel.setFrameStyle(frameStyle)
-        self.issueButton = QtGui.QPushButton("Issue" + Order.currency)
-
-        self.recipientNameLabel = QtGui.QLabel()
-        self.recipientNameLabel.setFrameStyle(frameStyle)
-        self.recipientNameButton = QtGui.QPushButton("set recipient name")
-       
-        self.amountLabel = QtGui.QLabel()
-        self.amountLabel.setFrameStyle(frameStyle)
-        self.amountButton = QtGui.QPushButton("amount")
-        
-        self.issueToAccountLabel = QtGui.QLabel()
-        self.issueToAccountLabel.setFrameStyle(frameStyle)
-        self.issueToAccountButton = QtGui.QPushButton("Send to receiving account")
-        
-        self.chooseCurrencyLabel = QtGui.QLabel()
-        self.chooseCurrencyLabel.setFrameStyle(frameStyle)
-        self.chooseCurrencyButton = QtGui.QPushButton("Set Token Name")
         
         self.getInfoLabel = QtGui.QLabel()
         self.getInfoLabel.setFrameStyle(frameStyle)
+        
+#        self.openFileNameLabel = QtGui.QLabel()
+#        self.openFileNameLabel.setFrameStyle(frameStyle)
+        self.openFileNameButton = QtGui.QPushButton("set Contract Steps")
+
+#        self.issueLabel = QtGui.QLabel()
+#        self.issueLabel.setFrameStyle(frameStyle)
+        self.issueButton = QtGui.QPushButton("Issue" + Order.currency)
+
+#        self.recipientNameLabel = QtGui.QLabel()
+#        self.recipientNameLabel.setFrameStyle(frameStyle)
+        self.recipientNameButton = QtGui.QPushButton("set recipient name")
+       
+#        self.amountLabel = QtGui.QLabel()
+#        self.amountLabel.setFrameStyle(frameStyle)
+        self.amountButton = QtGui.QPushButton("amount")
+        
+#        self.issueToAccountLabel = QtGui.QLabel()
+#        self.issueToAccountLabel.setFrameStyle(frameSt self.setMinimumSize(1280, 1024)yle)
+        self.issueToAccountButton = QtGui.QPushButton("Send to receiving account")
+        
+#        self.chooseCurrencyLabel = QtGui.QLabel()
+#        self.chooseCurrencyLabel.setFrameStyle(frameStyle)
+        self.chooseCurrencyButton = QtGui.QPushButton("Set Token Name")
+        
+#        self.getInfoLabel = QtGui.QLabel()
+#        self.getInfoLabel.setFrameStyle(frameStyle)
         self.getInfoButton = QtGui.QPushButton("GetInfo")
         
+        self.getBalanceButton = QtGui.QPushButton("Get Balance")
     
         self.getAccountDetailsButton = QtGui.QPushButton("Get Account Details")
         
         
-
+        self.getBalanceButton.clicked.connect(self.getBalance)    
         self.getAccountDetailsButton.clicked.connect(self.getAccountDetails)
         self.getInfoButton.clicked.connect(self.getInfo)
         self.stopButton.clicked.connect(self.stopChain)
@@ -203,78 +210,62 @@ class Dialog(QtGui.QDialog):
         self.chooseCurrencyButton.clicked.connect(self.chooseCurrency)
          
         self.native = QtGui.QCheckBox()
-        self.native.setText("Create Token")
+        self.native.setText("EZEOS")
         self.native.setChecked(True)
         if sys.platform not in ("win32", "darwin"):
             self.native.hide()
 
         layout = QtGui.QGridLayout()
         
-        layout.addWidget(self.setWalletNameButton, 0, 0)
-        layout.addWidget(self.setWalletNameLabel, 0, 1)
-        layout.addWidget(self.createWalletButton, 0, 2)
-        layout.addWidget(self.createWalletLabel, 0, 3)
-        layout.addWidget(self.setOwnerKeyButton, 2, 0)
-        layout.addWidget(self.setOwnerKeyLabel, 2, 1)
-        layout.addWidget(self.setActiveKeyButton, 2, 2)
-        layout.addWidget(self.setActiveKeyLabel, 2, 3)
-        layout.addWidget(self.importPrivateKeysButton, 4, 0)
-        layout.addWidget(self.importPrivateKeysLabel, 4, 1)
-        layout.addWidget(self.setAccountNameButton, 4, 2)
-        layout.addWidget(self.setAccountNameLabel, 4, 3)
-        layout.addWidget(self.createAccountButton, 6, 0)
-        layout.addWidget(self.createAccountLabel, 6, 1)
-        layout.addWidget(self.openContractButton, 6, 2)
-        layout.addWidget(self.openContractLabel, 6, 3)
-        layout.addWidget(self.openFileNameButton, 8, 0)
-        layout.addWidget(self.openFileNameLabel, 8, 1)
-        layout.addWidget(self.chooseCurrencyButton, 8, 2)
-        layout.addWidget(self.chooseCurrencyLabel, 8, 3)
-        layout.addWidget(self.issueButton, 10, 0)
-        layout.addWidget(self.issueLabel, 10, 1)
-        layout.addWidget(self.recipientNameLabel, 10, 3)
-        layout.addWidget(self.recipientNameButton, 10, 2)
-        layout.addWidget(self.amountLabel, 12, 1)
-        layout.addWidget(self.amountButton, 12, 0)
-        layout.addWidget(self.issueToAccountButton, 12, 2)
-        layout.addWidget(self.issueToAccountLabel, 12, 3)
+        layout.addWidget(self.issueToAccountButton, 7, 2)
+        layout.addWidget(self.amountButton, 7, 1)
+        layout.addWidget(self.recipientNameButton, 7, 0)        
+        layout.addWidget(self.issueButton, 6, 3)
+        layout.addWidget(self.openFileNameButton, 6, 2)
+        layout.addWidget(self.chooseCurrencyButton, 6, 1)
+        layout.addWidget(self.openContractButton, 6, 0)
+        layout.addWidget(self.getBalanceButton, 5, 3)
+        layout.addWidget(self.getAccountDetailsButton, 5, 2)
+        layout.addWidget(self.setAccountNameButton, 5, 0)
+        layout.addWidget(self.createAccountButton, 5, 1)
+        layout.addWidget(self.importPrivateKeysButton, 4, 4)
+        layout.addWidget(self.setOwnerKeyButton, 4, 3)
+        layout.addWidget(self.setActiveKeyButton, 4, 2)
+        layout.addWidget(self.createWalletButton, 4, 1)
+        layout.addWidget(self.setWalletNameButton, 4, 0)    
+        layout.addWidget(self.flushButton, 1, 4 )                
+        layout.addWidget(self.restartButton, 1, 3)
+        layout.addWidget(self.startButton, 1, 2)
+        layout.addWidget(self.stopButton, 1, 1)
+        layout.addWidget(self.getInfoButton, 1, 0)
+        layout.addWidget(self.getInfoLabel,  0, 0, 1, 6)
        
-        layout.addWidget(self.getAccountDetailsButton, 13, 0)
-        layout.addWidget(self.restartButton, 14, 0)
-        layout.addWidget(self.restartLabel, 14, 1)
-        layout.addWidget(self.startLabel, 15, 1)
-        layout.addWidget(self.startButton, 15, 0)
-        layout.addWidget(self.stopLabel, 16, 1)
-        layout.addWidget(self.stopButton, 16, 0)
-        layout.addWidget(self.flushButton, 17,0 )
-        layout.addWidget(self.flushLabel, 17, 1)
-        layout.addWidget(self.getInfoButton, 18, 0)
-        layout.addWidget(self.getInfoLabel, 18, 1)
-      
+        
         
         self.setLayout(layout)
-
         self.setWindowTitle("EZEOS")
         self.showMaximized()
+        
+        
     def stopChain(self):
-        flushAllFields(self)
+        
         subprocess.check_output(['killall','nodeos'])   
-        self.stopLabel.setText("Chain Stopped")
+        self.getInfoLabel.setText('chain stopped')
         
         
     def startChain(self):
-        flushAllFields(self)
+      
 
         out = subprocess.Popen(['xterm', '-e', 'nodeos --resync'])
         home = os.environ['HOME'] 
         os.environ['EOS_SOURCE'] = home + "/eos"
         os.environ['EOS_NODEOS']= home + ".local/share/eosio/nodeos/"
-        self.startLabel.setText(out)
+        self.getInfoLabel.setText('chain started' + out )
         
     def resetChain(self):
-        flushAllFields(self)
+      
         out = subprocess.check_output(['rm', '-rf', '$EOS_NODEOS' + 'data'])
-        self.restartLabel.setText(out)
+        self.getInfoLabel.setText('chain reset' + out)
         
     def setWalletName(self):
         text, ok = QtGui.QInputDialog.getText(self, "QInputDialog.getText()",
@@ -282,9 +273,8 @@ class Dialog(QtGui.QDialog):
                 QtCore.QDir.home().dirName())
         if ok and text != '':
             Wallet.name = text
-            self.setWalletNameLabel.setText(text)
-        
-                
+            self.getInfoLabel.setText(text)
+       
     def createWallet(self):
         createAccount = os.environ['HOME'] + '/eosio-wallet'
         print('***' + createAccount + '***')
@@ -295,7 +285,7 @@ class Dialog(QtGui.QDialog):
         f = open( Wallet.name, 'w' )
         f.write(out)
         f.close()
-        self.createWalletLabel.setText(out)
+        self.getInfoLabel.setText(out)
         
 
     def setOwnerKey(self):    
@@ -306,10 +296,10 @@ class Dialog(QtGui.QDialog):
         key2 = key2[:-1]
         f = open( Wallet.name + "OwnerKeys", 'w' )
         f.write(key)
-        f.close()
         Wallet.OwnerKey1 = key
         Wallet.PublicKey1 = key2
-        self.setOwnerKeyLabel.setText(out)
+        self.getInfoLabel.setText(out)
+       
 
     def setActiveKey(self):
         out = subprocess.check_output(['cleos', 'create', 'key'])
@@ -322,12 +312,12 @@ class Dialog(QtGui.QDialog):
         f.close()
         Wallet.OwnerKey2 = key
         Wallet.PublicKey2 = key2
-        self.setActiveKeyLabel.setText(out)
-
+        self.getInfoLabel.setText(out)
+        
     def importPrivateKeys(self):
         out = subprocess.check_output(['cleos', 'wallet', 'import', '-n', Wallet.name, Wallet.OwnerKey1])
         out1 = subprocess.check_output(['cleos', 'wallet', 'import', '-n', Wallet.name, Wallet.OwnerKey2])
-        self.importPrivateKeysLabel.setText(Wallet.OwnerKey1 +"\n"+ Wallet.OwnerKey2 + "\n" + out + "\n" + out1)
+        self.getInfoLabel.setText(Wallet.OwnerKey1 +"\n"+ Wallet.OwnerKey2 + "\n" + out + "\n" + out1)
         
     def createAccountName(self):
         text, ok = QtGui.QInputDialog.getText(self, "QInputDialog.getText()",
@@ -335,24 +325,26 @@ class Dialog(QtGui.QDialog):
                 QtCore.QDir.home().dirName())
         if ok and text != '':
             Account.name = text
-            self.setAccountNameLabel.setText(text)
-    
+            self.getInfoLabel.setText(text)
+        
     def createAccount(self): 
         out = subprocess.check_output(['cleos', 'create', 'account', 'eosio', Account.name, Wallet.PublicKey1, Wallet.PublicKey2])
-        self.createAccountLabel.setText(out)
+        self.getInfoLabel.setText(out)
     
     def loadTokenContract(self):
         options = QtGui.QFileDialog.DontResolveSymlinks | QtGui.QFileDialog.ShowDirsOnly
         directory = QtGui.QFileDialog.getExistingDirectory(self,
                 "QFileDialog.getExistingDirectory()",
-                self.openContractLabel.text(), options)
+                self.getInfoLabel.text(), options)
         if directory:
             Order.contract = directory
-        self.openContractLabel.setText(directory)
+            Order.contractAccountName = os.path.basename(directory)
+            
+        self.getInfoLabel.setText(directory)
         
     def setContractSteps(self):
         out = subprocess.check_output(['cleos', 'set', 'contract', Account.name,  Order.contract, '-p', Account.name ])
-        self.openFileNameLabel.setText(out)
+        self.getInfoLabel.setText(out)
 
     def chooseCurrency(self):
         text, ok = QtGui.QInputDialog.getText(self, "QInputDialog.getText()",
@@ -360,7 +352,7 @@ class Dialog(QtGui.QDialog):
                 QtCore.QDir.home().dirName())
         if ok and text != '':
             Order.currency = ' ' + text
-            self.chooseCurrencyLabel.setText(text)     
+            self.getInfoLabel.setText(text)
 
     
     def issueCurrency(self):
@@ -372,7 +364,7 @@ class Dialog(QtGui.QDialog):
         finalToken = token1 + token2 + token3 + token4 + token5
         print(finalToken)
         out = subprocess.check_output(['cleos', 'push', 'action', Account.name, 'create', finalToken, '-p', Account.name + '@active']) 
-        self.issueLabel.setText(out)
+        self.getInfoLabel.setText(out)
     
     def setRecipientName(self):
         text, ok = QtGui.QInputDialog.getText(self, "QInputDialog.getText()",
@@ -380,7 +372,7 @@ class Dialog(QtGui.QDialog):
                 QtCore.QDir.home().dirName())
         if ok and text != '':
             Order.name = text
-            self.recipientNameLabel.setText(text)
+            self.getInfoLabel.setText(text)
     
     def setAmount(self):
         text, ok = QtGui.QInputDialog.getText(self, "QInputDialog.getText()",
@@ -388,9 +380,13 @@ class Dialog(QtGui.QDialog):
                 QtCore.QDir.home().dirName())
         if ok and text != '':
             Order.amount = text + Order.currency
-            self.amountLabel.setText(text + Order.amount)     
+            self.amountLabel.setText(Order.amount)
+            self.getInfoLabel.setText(Order.amount)
     
     def issueToAccount(self):
+        
+        out = subprocess.check_output(['cleos', 'get', 'account', Account.name ])
+        self.getInfoLabel.setText(out)
         #cleos push action eosio.token issue '{"to": "scott", "quantity": "900.0000 EOS", "memo": "testing"}' -p eosio.token@active
         token1 = '{"to": "'
         token2 = Order.name
@@ -399,12 +395,11 @@ class Dialog(QtGui.QDialog):
         token5 = '", "memo": "testing"}'
         finalToken = token1 + token2 + token3 + str(token4) + token5
         out = subprocess.check_output(['cleos', 'push', 'action', Account.name, 'issue', finalToken, '-p', Account.name]) # + '@active']) 
-        self.issueToAccountLabel.setText(out)
+        self.getInfoLabel.setText(out)
     
     def flushWallets(self):
-        flushAllFields(self)
         subprocess.check_output(['rm', '-rf', os.environ['HOME'] + '/eosio-wallet/'])
-        self.flushLabel.setText("Deleted wallets")
+        self.getInfoLabel.setText("Deleted Wallets")
         
     def getInfo(self):
         out = subprocess.check_output(['cleos', 'get', 'info'])
@@ -415,26 +410,16 @@ class Dialog(QtGui.QDialog):
         out = subprocess.check_output(['cleos', 'get', 'account', Account.name ])
         self.getInfoLabel.setText(out)
     
-def flushAllFields(self): 
-        self.stopLabel.setText("") 
-        self.startLabel.setText("") 
-        self.restartLabel.setText("") 
-        self.setWalletNameLabel.setText("") 
-        self.createWalletLabel.setText("") 
-        self.setOwnerKeyLabel.setText("") 
-        self.setActiveKeyLabel.setText("") 
-        self.importPrivateKeysLabel.setText("") 
-        self.setAccountNameLabel.setText("") 
-        self.createAccountLabel.setText("") 
-        self.openContractLabel.setText("") 
-        self.openFileNameLabel.setText("") 
-        self.issueLabel.setText("") 
-        self.flushLabel.setText("") 
-        self.amountLabel.setText("") 
-        self.recipientNameLabel.setText("") 
-        self.issueToAccountLabel.setText("") 
+    def getBalance(self):
+        #cmd = 'cleos ' + 'get ' + 'currency ' + 'balance ' + Account.name + ' ' + Order.contractAccountName + ' ' + Order.currency 
+        #print(cmd)
+        out = subprocess.check_output(['cleos', 'get', 'currency', 'balance', Order.contractAccountName, Account.name,  Order.currency ])
+        self.getInfoLabel.setText(out)
+    
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
+
     dialog = Dialog()
     sys.exit(dialog.exec_())
